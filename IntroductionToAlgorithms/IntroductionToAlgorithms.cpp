84235f5fbd389e1f79d24b62c4c20b24e0df9e92
唐韬 <inserlan@163.com>
2019-02-19 22:50:15 +0800

    implement merge sort

--- IntroductionToAlgorithms/IntroductionToAlgorithms.cpp
+++ IntroductionToAlgorithms/IntroductionToAlgorithms.cpp
@@ -8,7 +8,8 @@
 int main()
 {
 	int nArray[] = { 1,3,5,6,4,2,5,9,7,4,5,3,32,4,3,2,4,3,32,65,8,7,54,88 };
-	InsertionSort(nArray, 24);
+	//InsertionSort(nArray, 24);
+	MergeSort(nArray, 0, 23);
 	for (auto v : nArray)
 		std::cout << v << ' ';
 	std::cout << "\n";
--- IntroductionToAlgorithms/Sort.h
+++ IntroductionToAlgorithms/Sort.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// insertion sort
+// O(n^2)
 template<typename T>
 void InsertionSort(T* pArray, int nSize)
 {
@@ -13,5 +15,44 @@ void InsertionSort(T* pArray, int nSize)
 			i--;
 		}
 		pArray[i + 1] = curr;
+	}
+}
+
+
+// merge sort
+// O(n*log(n))
+template<typename T>
+void Merge(T* pArray, int left, int mid, int right)
+{
+	T* pTempArray = new T[right - left + 1];
+	int pos = 0, lpos = left, rpos = mid + 1;
+	// sort and put into temp array
+	while ((lpos <= mid) && (rpos <= right))
+	{
+		if (pArray[lpos] < pArray[rpos])
+			pTempArray[pos++] = pArray[lpos++];
+		else
+			pTempArray[pos++] = pArray[rpos++];
+	}
+	while (lpos <= mid)
+		pTempArray[pos++] = pArray[lpos++];
+	while (rpos <= right)
+		pTempArray[pos++] = pArray[rpos++];
+	// copy
+	for (int i = 0; i <= (right - left); i++)
+		pArray[left + i] = pTempArray[i];
+
+	delete[] pTempArray;
+}
+
+template<typename T>
+void MergeSort(T* pArray, int left, int right)
+{
+	if (left < right)
+	{
+		int mid = (left + right) / 2;
+		MergeSort(pArray, left, mid);
+		MergeSort(pArray, mid + 1, right);
+		Merge(pArray, left, mid, right);
 	}
 }
